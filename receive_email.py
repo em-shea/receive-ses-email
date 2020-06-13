98c6b19cfd0c856d7873eb1c704eb80e887b92cf
em-shea <c.emilyshea@gmail.com>
2020-06-13 11:44:25 -0700

    add s3 bucket event

--- receive_email.py
+++ receive_email.py
@@ -2,25 +2,32 @@ import os
 import json
 import boto3
 
+s3_client = boto3.client('s3')
 sns_client = boto3.client('sns')
 
 def lambda_handler(event, context):
 
-  email_content = event['Records'][0]['ses']['mail']['commonHeaders']
+  print(event)
 
-  print(email_content)
+  # email_content = event['Records'][0]['ses']['mail']['commonHeaders']
 
-  try:
-    publish_sns_update(email_content)
-    print("Message send success.")
-  except Exception as error:
-    print(f"Message send failed, error: {error}")
+  # print(email_content)
+
+  # try:
+  #   publish_sns_update(email_content)
+  #   print("Message send success.")
+  # except Exception as error:
+  #   print(f"Message send failed, error: {error}")
 
 def publish_sns_update(email_content):
 
+  content_string = json.dumps(email_content)
+
+  email_body = ""
+
   response = sns_client.publish(
       TargetArn = os.environ['SNS_TOPIC_ARN'], 
-      Message=json.dumps({'default': email_content}),
+      Message=json.dumps({'default': content_string}),
       MessageStructure='json'
   )
 
